a44147e286ef365bd00852f210f692b52a9f7ada
wesley <wesley@wd-media.nl>
2016-02-06 20:37:57 -0500

    Add CORS

--- html/api/index.php
+++ html/api/index.php
@@ -1,8 +1,21 @@
 <?php
+// CORS when your origin is not localhost you are going to need this or else it will not work.
+// if you are using it outside of your home network please reconsider the * of allow origin
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, PUT, DELETE, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, auth, user");
+header("Access-Control-Max-Age: 1728000");
 
 // Load the composer autoloader
 require __DIR__ . '/../../vendor/autoload.php';
 
+\Rhonda\Config:: load_file("config","../../etc/config.json");
+
+\Rhonda\Config:: load_file("config","../../etc/config.json");
+\Rhonda\Config:: load_file("config","../../etc/config.json");
+
+\Rhonda\Config:: load_file("config","../../etc/config.json");
+\Rhonda\Config:: load_file("config","../../etc/config.json");
 \Rhonda\Config:: load_file("config","../../etc/config.json");
 
 
--- html/api/routes.php
+++ html/api/routes.php
@@ -1,10 +1,6 @@
 <?php
 
-// Custom 404 Handler
-$router->set404(function() {
-  header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-  echo '404, route not found!';
-});
+
 
 
 /***************/
@@ -13,10 +9,10 @@ $router->set404(function() {
 $router->mount('/gpio', function() use ($router) {
 
   // GPIO Write command :pin :value (0/1)
-  $router->post('/write/(\S+)/(\S+)/','\Controllers\GPIOCtl::write');
+  $router->put('/write/(\S+)/(\S+)/','\Controllers\GPIOCtl::write');
 
   // GPIO Mode command :pin :value (IN or OUT)
-  $router->post('/mode/(\S+)/(\S+)/','\Controllers\GPIOCtl::mode');
+  $router->put('/mode/(\S+)/(\S+)/','\Controllers\GPIOCtl::mode');
 });
 
 /***************/
@@ -25,5 +21,14 @@ $router->mount('/gpio', function() use ($router) {
 $router->mount('/rfid', function() use ($router) {
 
   // RFID send command :value 
-  $router->post('/send/(\S+)/','\Controllers\RFIDCtl::send');
+  $router->put('/send/(\S+)/','\Controllers\RFIDCtl::send');
+});
+
+
+$router->options('/(\S+)','');
+
+// Custom 404 Handler
+$router->set404(function() {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+  echo '404, route not found!';
 });
