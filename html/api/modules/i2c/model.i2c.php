664b00b259141d0f4cd42d47bddf571ddeee78b1
wesley <wesley@wd-media.nl>
2016-02-07 15:57:23 -0500

    change model

--- html/api/modules/i2c/model.i2c.php
+++ html/api/modules/i2c/model.i2c.php
@@ -48,7 +48,7 @@ class I2c{
     // Load the data out of the sensor
     error_log("cat ".$this->path."".$this->id."/w1_slave");
     exec("cat ".$this->path."".$this->id."/w1_slave", $this->sensor);
-    $sensor_info = explode('t=', $this->sensor);
+    $sensor_info = explode('t=', (string)$this->sensor);
     error_log(print_r($sensor_info,true));
     $sensor->celsius = $sensor_info[1]/1000;
     $sensor->fahrenheit = $this->celcius_to_fahrenheit($sensor->celsius);
