37e9908d830208a394f84c228420e195b5e118fc
wesley <wesley@wd-media.nl>
2016-02-13 21:20:52 -0500

    update

--- html/api/modules/onkyo/controller.onkyo.php
+++ html/api/modules/onkyo/controller.onkyo.php
@@ -84,7 +84,7 @@ public function command($type){
     error_log(print_r($body, true));
     $socket = 'tcp://'.$body->ip.':'.$body->port;
     error_log($socket);
-    $fp = \@stream_socket_client($socket, '', '', 10);
+    $fp = @stream_socket_client($socket, 0, '', 10);
     if(!$fp) {
       throw new \Exception("Failed to make a connection");
     }
